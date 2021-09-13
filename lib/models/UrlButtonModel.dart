00a7ef4cbb2b18f6475fad6f90ecb3deb8f5f09e
kwanth <59208523+kwan19961217@users.noreply.github.com>
2021-09-13 23:08:04 +0800

    Add Email Button

--- lib/models/UrlButtonModel.dart
+++ lib/models/UrlButtonModel.dart
@@ -1,12 +1,14 @@
 class UrlButtonModel {
   static const List<String> _icons = [
     'assets/icons/github.png',
-    'assets/icons/linkedin_small.png'
+    'assets/icons/linkedin_small.png',
+    'assets/icons/email.png',
   ];
 
   static const List<String> _urls = [
     'https://github.com/kwan19961217',
-    'https://www.linkedin.com/in/tsz-hong-kwan-4a6522194/'
+    'https://www.linkedin.com/in/tsz-hong-kwan-4a6522194/',
+    'mailto:kwan19961217@gmail.com'
   ];
 
   static List<String> get getIcons => _icons;
